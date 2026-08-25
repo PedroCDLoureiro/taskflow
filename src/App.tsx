540bed5a4840383293be364d59696fd49eda3396
Pedro Loureiro <pedroloureiro_1912@hotmail.com>
2026-08-24 23:33:26 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <main>
+            <h1>TaskFlow</h1>
+            <p>Gerenciamento de projetos e tarefas.</p>
+        </main>
+    );
+}
+
+export default App;
